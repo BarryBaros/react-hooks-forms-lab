5db9ca9351ecf5527ca5c63c8a7ef415589fecc4
Timothy Barasa <timbaros87@gmail.com>
2024-05-23 22:31:28 +0300

    Modified ItemForm

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,10 @@ import ShoppingList from "./ShoppingList";
 import Header from "./Header";
 import itemData from "../data/items";
 import Filter from "./Filter";
+import ItemForm from "./ItemForm";
 
 function App() {
+  const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [search, setSearch] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("All");
@@ -21,6 +23,20 @@ function App() {
     setSelectedCategory(event.target.value);
   }
 
+  function handleItemFormSubmit(newItem) {
+    setItems((items) => [...items, newItem]);
+  }
+
+   const itemsToDisplay = items.filter((item) => {
+     if (selectedCategory !== "All" && item.category !== selectedCategory) {
+       return false;
+     }
+     if (search && !item.name.toLowerCase().includes(search.toLowerCase())) {
+       return false;
+     }
+     return true;
+   });
+  
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
@@ -28,8 +44,9 @@ function App() {
         onSearchChange={handleSearchChange}
         onCategoryChange={handleCategoryChange}
       />
+      <ItemForm onItemFormSubmit={handleItemFormSubmit} />
       <ShoppingList
-        items={itemData}
+        items={itemsToDisplay}
         search={search}
         selectedCategory={selectedCategory}
       />
